4b9cec0d752afd5272fb49c243fa54a1637edbab
Pavel Tsikhanau <tishka1991@gmail.com>
2024-03-04 23:38:11 +0300

    feat: add popup layout & popup function

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 let weight = document.querySelector("#weight");
 let height = document.querySelector("#height");
 let button = document.querySelector(".input-button");
+let popUp = document.querySelector('.popup');
+let popUpContent = document.querySelector('.popup-content')
 
 function ObesityCalculation() {
   let res = weight.value / Math.pow(height.value, 2);
-  console.log(res)
+  function popUpFunc() {
+    popUp.style.visibility = 'visible';
+    if (res < 18.5) {
+      popUpContent.innerText = `You are a winner! Your total score is ${score + 5}!`;
+    }
+    
+  }
 };
+
+popUp.addEventListener('click', function(e) {
+  popUp.style.visibility = 'hidden';
+});
 
 button.addEventListener("click", ObesityCalculation)
